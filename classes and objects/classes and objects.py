f81792feab71cf8d6921b556ba82bb4f90f7e707
yusuf bouaouina <yusuf.bouaouina@gmail.com>
2026-08-03 14:48:16 +0300

    Delete classes and objects directory

--- classes and objects/classes and objects.py
+++
@@ -1,8 +0,0 @@
-class mob():
-    def __init__(self, hp, dmg, speed):
-        self.hp = hp
-        self.dmg = dmg
-        self.speed = speed
-
-creeper = mob(2,3,4.5)
-print(creeper.hp, creeper.dmg, creeper.speed)
